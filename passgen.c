f3b0a6be136cbf61334e9fad4c1ab5d6a0ef7e00
Nate Eaton <nateat9@gmail.com>
2023-07-25 11:44:29 -0700

    Better fix to random value range

--- passgen.c
+++ passgen.c
@@ -4,14 +4,14 @@
 
 int main(){
   int len = 16;
-  char pass[len];
+  unsigned char pass[len];
   int check = getrandom(pass, len, GRND_RANDOM);
   if(check != len){
     perror("Failed to access random values\n");
     return -1;
   }
   for(int i = 0; i < len; i++){
-    pass[i] = 33 + ((pass[i]+128)%93);
+    pass[i] = 33 + ((pass[i])%93);
   }
   printf("%s\n",pass);
   return 0;
