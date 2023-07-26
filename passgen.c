58adc0896894411aca61374d334e409fb939f93c
Nate Eaton <nateat9@gmail.com>
2023-07-26 09:32:51 -0700

    Added support for charsets using the -c and -C options

--- passgen.c
+++ passgen.c
@@ -1,28 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/random.h>
+
+#include "passgen.h"
+
+int chooseCustomPool(char* pool){
+  if(strcmp(pool, "lower") == 0)
+    return 0;
+  if(strcmp(pool, "upper") == 0)
+    return 1;
+  if(strcmp(pool, "letter") == 0)
+    return 2;
+  if(strcmp(pool, "alphanum") == 0)
+    return 3;
+  if(strcmp(pool, "number") == 0)
+    return 4;
+  return -1;
+}
 
 int main(int argc, char **argv){
   //set default length
   int len = 16;
+  char* pool = ASCII;
   //handle command line args
   int opt;
-  while((opt = getopt(argc, argv, "hl:")) != -1){
+  char customPool[255] = "";
+  while((opt = getopt(argc, argv, "hc:C:l:")) != -1){
     switch(opt){
       case 'h':
         printf("help menu\n");
         return 0;
+      case 'c':
+        switch(chooseCustomPool(optarg)){
+          case 0: strcat(customPool, LOWER); break;
+          case 1: strcat(customPool, UPPER); break;
+          case 2: strcat(customPool, LETTER); break;
+          case 3: strcat(customPool, LETTER); strcat(customPool, NUMBER); break;
+          case 4: strcat(customPool, NUMBER); break;
+          default: printf("invalid charset: %s\nplease use \'-C\' to set explicit charset\n", optarg); return -1;
+        }
+        break;
+      case 'C':
+        strcpy(customPool, optarg);
+        break;
       case 'l':
         sscanf(optarg, "%d", &len);
+        if(len < 0) len = 16;
         break;
       case ':':
         printf("option needs a value\n");
         break;
       case '?':
-        printf("unknown option: %c\n", optopt);
-        break;
+        return -1;
     }
+  }
+  if(strcmp(customPool, "") != 0){
+    pool = customPool;
   }
   unsigned char pass[len+1];
   int check = getrandom(pass, len, GRND_RANDOM);
@@ -31,7 +66,8 @@ int main(int argc, char **argv){
     return -1;
   }
   for(int i = 0; i < len; i++){
-    pass[i] = 33 + ((pass[i])%93);
+    //pass[i] = 33 + ((pass[i])%93);
+    pass[i] = pool[pass[i]%strlen(pool)];
   }
   pass[len] = '\0';
   printf("%s\n",pass);
---
+++ passgen.h
@@ -0,0 +1,6 @@
+static char* LOWER = "abcdefghijklmnopqrstuvwxyz";
+static char* UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+static char* LETTER = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+static char* NUMBER = "1234567890";
+static char* SYMBOL = "!\"#$%&\'()*()+,-./:;<=>?@[\\]^_`{|}~";
+static char* ASCII = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()*()+,-./:;<=>?@[\\]^_`{|}~1234567890";
